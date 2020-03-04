f3009f234e82e769bfae643b6ffcaf212ed6e3b8
kamran saberifard <kamisaberi@yahoo.com>
2020-03-05 02:36:23 +0330

    first commit

--- 01.The.sum.of.first.10.natural.numbers.py
+++ 01.The.sum.of.first.10.natural.numbers.py
@@ -6,3 +6,7 @@
 # .
 # .
 # s += 10
+s = 0
+for i in range(1, 11):
+    s += i
+print(s)
--- 02.HCF.of.2.natural.numbers.py
+++ 02.HCF.of.2.natural.numbers.py
@@ -1,3 +1,13 @@
 a = 12
-b = 9
+b = 11
 # get minimum of a and b
+if a > b:
+    m = b
+else:
+    m = a
+hcf = 1
+for i in range(m, 0, -1):
+    if a % i == 0 and b % i == 0:
+        hcf = i
+        break
+print(hcf)
